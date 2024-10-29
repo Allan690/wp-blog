0f1b914f2b59624776128231738643effcca4e74
Hush <allannelly690@gmail.com>
2024-10-30 00:43:37 +0300

    chore: wordpress blog infra

---
+++ infra/nginx/nginx.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# continue normal Nginx startup
+exec /docker-entrypoint.sh "$@"
---
+++ services.sh
@@ -0,0 +1,4 @@
+export REPO=docker.io
+export TAG=dev
+
+docker compose up --build -d wordpress nginx mysql
