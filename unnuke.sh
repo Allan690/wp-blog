61208bc1def94a1ad692ffe11b894abc5d896570
Hush <allannelly690@gmail.com>
2024-11-01 10:18:31 +0300

    unnuke wp changes

---
+++ unnuke.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINER_NAME="wp-blog-mysql-1"
+DB_NAME="wordpress"
+DB_USER="admin"
+DB_PASSWORD="admin"
+NEW_URL="https://blog.hushdev.xyz/wp"
+
+docker exec -i $CONTAINER_NAME mariadb -u$DB_USER -p$DB_PASSWORD $DB_NAME <<EOF
+UPDATE wp_options SET option_value = '$NEW_URL' WHERE option_name = 'home';
+UPDATE wp_options SET option_value = '$NEW_URL' WHERE option_name = 'siteurl';
+SELECT option_name, option_value FROM wp_options WHERE option_name IN ('home', 'siteurl');
+EOF
